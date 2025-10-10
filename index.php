380415fd0ff883ffc813b915019eb0289c8690ca
dira21 <raraajadeh21@gmail.com>
2025-10-10 08:54:17 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar web programming</title>
+</head>
+<body>
+    <?php
+        // echo "Hello World";
+
+        // variabel (menampung nilai)
+        $nilai1 = 70;
+        $nilai2 = 80;
+        $nama = "Dira";
+        $rata2 = ($nilai1 + $nilai2) / 2;
+
+        echo "Nama : <b>$nama</b>";
+        echo "<br>";
+        echo "nilai rata-ratanya adalah $rata2";
+
+        // ATURAN MEMBUAT VARIABEL di PHP
+        // 1. tidak boleh diawali oleh angka atau simbol kecuali _ underscore
+        // 2. tidak boleh pakai spasi
+        // 3. variable bersifat case sensitive
+
+        // OPERATOR ARITMATIKA (+ - * / ** %)
+    ?>
+</body>
+</html>
---
+++ logika.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /* struktur logika
+    if, elseif, else
+    switch
+
+    operator perbandingan (== != > < >= <=)
+    operator logika (and or) (&& ||) */
+
+    $nilai = 70;
+
+    if($nilai > 50) {
+        echo "kamu lulus";
+    }
+    ?>
+</body>
+</html>
