1115c5a7a107cc1288b2840c50a0222308be10d3
dira21 <raraajadeh21@gmail.com>
2026-01-29 15:19:29 +0700

    edit

--- biodata/siswa/proses_edit.php
+++ biodata/siswa/proses_edit.php
@@ -1,27 +1,59 @@
 <?php
-#1 Meng-koneksikan PHP ke MYSQL
-include("../koneksi.php"); 
+    #1. Meng-koneksikan PHP ke MySQL
+    include("../koneksi.php");
 
-#2 Mengambil value dari form tambah
-$id = $_POST['id'];
-$nama = $_POST['nama'];
-$nisn = $_POST['nisn'];
-$tp_lahir = $_POST['tp_lahir'];
-$tg_lahir = $_POST['tg_lahir'];
-$alamat = $_POST['alamat'];
-$email = $_POST['email'];
-$jk = $_POST['jk'];
-$jur = $_POST['jur'];
+    #2. Mengambil Value dari Form Tambah
+    $id = $_POST['id'];
+    $nama = $_POST['nama'];
+    $nisn = $_POST['nisn'];
+    $tp_lahir = $_POST['tp_lahir'];
+    $tg_lahir = $_POST['tg_lahir'];
+    $alamat = $_POST['alamat'];
+    $email = $_POST['email'];
+    $jk = $_POST['jk'];
+    $jur = $_POST['jur'];
+    $nama_foto = $_FILES['foto']['name'];
+    $tmp_foto = $_FILES['foto']['tmp_name'];
 
-#3 Query Insert(proses tambah data)
-$query = "UPDATE biodata SET nama='$nama', nisn='$nisn', tp_lahir='$tp_lahir', tg_lahir='$tg_lahir', alamat='$alamat', email='$email', jk='$jk', jur='$jur' WHERE id='$id'";
+    if($nama_foto != ""){
+        #hapus foto lama
+        $qry = "SELECT * FROM biodata WHERE id = '$id'";
+        $hapus_foto = mysqli_query($koneksi,$qry);
+        $data = mysqli_fetch_array($hapus_foto);
+        $nama_foto_hapus = $data['foto'];
+        $lokasi_foto = "../fotosiswa/".$nama_foto_hapus;
 
-$edit = mysqli_query($koneksi, $query);
+        if(file_exists($lokasi_foto)){
+            unlink($lokasi_foto);
+        }
 
-#4 Jika berhasil triggernya apa?
-if($edit){
-    header("location:index.php");
-}else{
-    echo "Data gagal diedit";
-}
-?> 
+        #3. Query Insert (proses edit data)
+        $query = "UPDATE biodata SET nama='$nama', nisn='$nisn', tp_lahir='$tp_lahir', 
+        tg_lahir='$tg_lahir', alamat='$alamat', email='$email', jk='$jk',  jur='$jur', foto='$nama_foto' 
+        WHERE id='$id'";
+
+        #hapus foto
+        // $lokasi_foto = "../fotosiswa/$nama_foto";
+        // if(file_exists($lokasi_foto)){
+        //     unlink($lokasi_foto);
+        // }
+
+        #tambahkan foto
+        move_uploaded_file($tmp_foto,"../fotosiswa/$nama_foto");
+    }else{
+        #3. Query Insert (proses edit data)
+        $query = "UPDATE biodata SET nama='$nama', nisn='$nisn', tp_lahir='$tp_lahir', 
+        tg_lahir='$tg_lahir', alamat='$alamat', email='$email', jk='$jk',  jur='$jur' 
+        WHERE id='$id'";
+    }
+
+    
+    $edit = mysqli_query($koneksi,$query);
+
+    #4. Jika Berhasil triggernya apa? (optional)
+    if($edit){
+        header("location:index.php");
+    }else{
+        echo "Data Gagal diedit";
+    }
+?>
